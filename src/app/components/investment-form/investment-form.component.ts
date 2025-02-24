2286ed8ecbf22b83096eb4fbde237429819bcc31
Joshua Davis <joshuad100717@outlook.com>
2025-02-23 20:43:27 -0700

    scaffold project files

---
+++ src/app/components/investment-form/investment-form.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-investment-form',
+  imports: [],
+  templateUrl: './investment-form.component.html',
+  styleUrl: './investment-form.component.css'
+})
+export class InvestmentFormComponent {
+
+}
